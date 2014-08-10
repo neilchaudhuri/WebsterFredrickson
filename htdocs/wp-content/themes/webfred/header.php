efaeb6afc91fcd387bfa92fb9c27281903227605
Neil Chaudhuri <neil@vidyasource.com>
2014-08-09 20:04:27 -0400

    Remove circles; add 4 practice areas

--- htdocs/wp-content/themes/webfred/header.php
+++ htdocs/wp-content/themes/webfred/header.php
@@ -46,20 +46,19 @@
                 </div>
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#">About</a></li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Practice Areas</span></a>
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li class="divider"></li>
-                                <li class="dropdown-header">Nav header</li>
-                                <li><a href="#">Separated link</a></li>
-                                <li><a href="#">One more separated link</a></li>
+                                <li><a href="#">Employment Law</a></li>
+                                <li><a href="#">Bankruptcy Law</a></li>
+                                <li><a href="#">Whistleblower Protection</a></li>
+                                <li><a href="#">Commercial Law</a></li>
                             </ul>
+                        </li>
+                        <li><a href="#">News</a></li>
+                        <li>
+                            <a href="tel:1-202-659-8510">202-659-8510</a>
                         </li>
                     </ul>
                 </div>
